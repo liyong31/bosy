4e26ee726fde313b56a2607ea7815a5c678faa7b
Yong Li <duckly_lee@163.com>
2020-09-30 09:39:25 +0800

    add ltl2geba

--- Tools/ltl2geba.cc
+++ Tools/ltl2geba.cc
@@ -14,8 +14,8 @@
 #include <spot/tl/simplify.hh>
 #include <spot/twaalgos/translate.hh>
 #include <spot/twaalgos/hoa.hh>
+#include <spot/twaalgos/product.hh>
 #include <spot/twaalgos/remprop.hh>
-
 
 /*
 Translate a LTL formula to a universal co-Buchi automaton
@@ -25,8 +25,7 @@ We thus need to obtain the Buchi automaton for the formula \exists O' \phi(O'/O)
 using namespace spot;
 using namespace std;
 
-void
-ltlf2geba(const char *ltlf, const char* outs)
+void ltlf2geba(const char *ltlf, const char *outs, bool product)
 {
     //spot::bdd_dict_ptr dict = spot::make_bdd_dict();
     auto pf1 = spot::parse_infix_psl(ltlf);
@@ -47,9 +46,9 @@ ltlf2geba(const char *ltlf, const char* outs)
     string ins = outs;
     std::stringstream ss(ins);
     std::string token;
-    while (getline(ss, token, ',')) 
+    while (getline(ss, token, ','))
     {
-        if(! token.empty())
+        if (!token.empty())
         {
             out_aps.push_back(token);
             //cout << "output ap: " << token << endl;
@@ -57,25 +56,35 @@ ltlf2geba(const char *ltlf, const char* outs)
             m[formula::ap(token)] = formula::ap(p);
         }
     }
-    
+
     // create formula \phi(O'/O)
     formula fprime = relabel_apply(f, &m);
     //cout << "Duplicated formula is " << fprime << endl;
+    spot::twa_graph_ptr nba = nullptr;
 
-    // create formula \phi(O'/O) \land ! \phi
-    formula r = formula::And({fprime, formula::Not(f)});
+    if (!product)
+    {
+        // create formula \phi(O'/O) \land ! \phi
+        formula r = formula::And({fprime, formula::Not(f)});
+        // translation
+        spot::translator trans;
+        trans.set_type(spot::postprocessor::BA);
+        trans.set_pref(spot::postprocessor::Small);
+        nba = trans.run(r);
+    }else 
+    {
+        spot::translator trans;
+        trans.set_type(spot::postprocessor::BA);
+        trans.set_pref(spot::postprocessor::Small);
 
-    //cout << "Resulted formula is " << r << endl;
-    
-    // translation
-    spot::translator trans;
-    trans.set_type(spot::postprocessor::BA);
-    trans.set_pref(spot::postprocessor::Small);
-    spot::twa_graph_ptr nba = trans.run(r);
+        spot::twa_graph_ptr nba_left = trans.run(fprime);
+        spot::twa_graph_ptr nba_right = trans.run(formula::Not(f));
+        nba = spot::product(nba_left, nba_right);
+    }
 
     // quantify out all O' variables
     spot::remove_ap rem;
-    for(string out_ap : out_aps)
+    for (string out_ap : out_aps)
     {
         string p = out_ap + "_";
         rem.add_ap(p.c_str());
@@ -93,9 +102,17 @@ ltlf2geba(const char *ltlf, const char* outs)
 
 int main(int argc, char **argv)
 {
-    if (argc < 1 || argc > 3)
+    if (argc < 1 || argc > 4)
         std::cout << "Please input formula file and outputs" << std::endl;
     //std::string ltlf = argv[1];
-    ltlf2geba(argv[1], argv[2]);
+    if (argc > 3)
+    {
+        ltlf2geba(argv[1], argv[2], false);
+    }
+    else
+    {
+        ltlf2geba(argv[1], argv[2], true);
+    }
+
     return 0;
 }
